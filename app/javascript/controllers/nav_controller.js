6ddba254d5ce2f0baf2c2af1b6ee3d37cb884080
thomas hunckler <th@phytodia.com>
2024-12-12 10:46:24 +0100

    change

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="nav"
 export default class extends Controller {
-  static targets = [ "burger" ]
+  static targets = [ "burger","menu_mobile" ]
   connect() {
   }
   toggle(){
     this.burgerTarget.classList.toggle("is-active")
+    this.menu_mobileTarget.classList.toggle("is-active")
   }
 }
