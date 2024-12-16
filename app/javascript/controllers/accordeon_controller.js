9d5750830445f971fcac0ed2f384b82a7e09001b
thomas hunckler <th@phytodia.com>
2024-12-16 13:29:47 +0100

    change

--- app/javascript/controllers/accordeon_controller.js
+++ app/javascript/controllers/accordeon_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="accordeon"
 export default class extends Controller {
+  static targets = ["expand"]
   connect() {
+  }
+  toggle(event){
+    this.expandTargets.forEach((element)=>{
+      element.classList.remove("open")
+    })
+    event.currentTarget.nextElementSibling.classList.add("open")
   }
 }
