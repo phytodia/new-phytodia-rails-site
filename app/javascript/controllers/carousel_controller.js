7ed5e9212fb945c6450e38d8c051259e3ac88a31
thomas hunckler <th@phytodia.com>
2025-01-20 14:23:56 +0100

    change

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,15 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
-  static targets = ["left","right","carousel","card"]
+  static targets = ["left","right","carousel","card","point"]
   connect() {
   }
   clickRight(){
     let carousel = document.querySelector(".carousel");
     carousel.scrollLeft += document.querySelector(".card_carousel").clientWidth
+
+    let indexPoint = this.pointTargets.indexOf(document.querySelector(".carousel_point.active"))
+    this.pointTargets[indexPoint].classList.remove("active")
+    this.pointTargets[indexPoint+1].classList.add("active")
+    //ajouter condition if 0 et max de l'array
   }
   clickLeft(){
     let carousel = document.querySelector(".carousel");
     carousel.scrollLeft -= document.querySelector(".card_carousel").clientWidth
+
+    let indexPoint = this.pointTargets.indexOf(document.querySelector(".carousel_point.active"))
+    this.pointTargets[indexPoint].classList.remove("active")
+    this.pointTargets[indexPoint-1].classList.add("active")
+    //ajouter condition if 0 et max de l'array
   }
 }
