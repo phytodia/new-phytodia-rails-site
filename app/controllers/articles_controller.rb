4ba071729a6da6ba5e1ef2566552067668d9ab65
thomas hunckler <th@phytodia.com>
2025-02-13 15:53:57 +0100

    change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,9 +22,16 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.friendly.find(params[:id])
   end
 
   def update
+    @article = Article.friendly.find(params[:id])
+    @article.update(article_params)
+    authors = params[:article][:author_ids].reject { |c| c.empty? }
+    @authors = Author.where(id:authors)
+    @article.authors = @authors
+    @article.save
   end
 
   def destroy
