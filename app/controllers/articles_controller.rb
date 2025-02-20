1b694bfdcbc03c39603f3471a42fc7cb2a1b5105
thomas hunckler <th@phytodia.com>
2025-02-20 09:01:10 +0100

    change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,16 @@ class ArticlesController < ApplicationController
 
     add_breadcrumb "Blog", :blog_path
     add_breadcrumb "<span>#{@article.titre} canonical à mettre</span>".html_safe, :post_path
+
+    headings = JSON.parse(@article.content.body)["content"].select{|arr| arr["type"] === "heading"}
+    headings_2 = headings.select{|arr| arr["attrs"]["level"] === 2}
+
+    @summary = []
+
+    headings_2.each_with_index do |head, index|
+      head["attrs"].merge!(id:index) if !head["content"].nil?
+      @summary << {head: head["content"][0]["text"], index: index} if !head["content"].nil?
+    end
   end
 
   def new
@@ -39,31 +49,20 @@ class ArticlesController < ApplicationController
     @authors = Author.where(id:authors)
     @article.authors = @authors
 
-    headings = JSON.parse(@article.content.body)["content"].select{|arr| arr["type"] === "heading"}
-    headings_2 = headings.select{|arr| arr["attrs"]["level"] === 2}
+    #xx = ((JSON.parse(@article.content.body)["content"].select{|arr| arr["type"] === "heading"}).select{|arr| arr["attrs"]["level"] === 2}).each_with_index do |head, index|
+      #head["attrs"].merge!(id:index) if !head["content"].nil?
+    #end
 
-    summary = []
-    #JSON.parse(@article.content.body).to_json
+    #counter_ids = 0
+    #yy = JSON.parse(@article.content.body)
+    #yy["content"].each do |arr|
+    #  if arr["type"] === "heading" && arr["attrs"]["level"] === 2
+    #    arr["attrs"].merge!("id": counter_ids)
+    #    counter_ids += 1
+    #  end
+    #end
 
-    headings_2.each_with_index do |head, index|
-      head["attrs"].merge!(id:index) if !head["content"].nil?
-      summary << {head: head["content"][0]["text"], index: index} if !head["content"].nil?
-    end
-
-    xx = ((JSON.parse(@article.content.body)["content"].select{|arr| arr["type"] === "heading"}).select{|arr| arr["attrs"]["level"] === 2}).each_with_index do |head, index|
-      head["attrs"].merge!(id:index) if !head["content"].nil?
-    end
-
-    counter_ids = 0
-    yy = JSON.parse(@article.content.body)
-    yy["content"].each do |arr|
-      if arr["type"] === "heading" && arr["attrs"]["level"] === 2
-        arr["attrs"].merge!("id": counter_ids)
-        counter_ids += 1
-      end
-    end
-
-    @article.content.body = yy.to_json
+    #@article.content.body = yy.to_json
     #zz = JSON.parse(@article.content.body)
     #@article.content.body = yy.to_json
     @article.save!
