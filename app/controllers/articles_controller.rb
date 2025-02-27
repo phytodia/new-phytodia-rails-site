753ee2fe03bffd5d8822a8e5fbc59b60c10a2fac
thomas hunckler <th@phytodia.com>
2025-02-27 14:40:28 +0100

    change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,6 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.friendly.find(params[:id])
 
-    add_breadcrumb "Blog", :blog_path
-    add_breadcrumb "<span>#{@article.titre}</span>".html_safe
-
     headings = JSON.parse(@article.content.body)["content"].select{|arr| arr["type"] === "heading"}
     headings_2 = headings.select{|arr| arr["attrs"]["level"] === 2}
 
@@ -20,6 +17,9 @@ class ArticlesController < ApplicationController
       head["attrs"].merge!(id:index) if !head["content"].nil?
       @summary << {head: head["content"][0]["text"], index: index} if !head["content"].nil?
     end
+
+    add_breadcrumb "Blog", :blog_path
+    add_breadcrumb "<span>#{@article.titre}</span>".html_safe
   end
 
   def new
@@ -65,6 +65,11 @@ class ArticlesController < ApplicationController
     #@article.content.body = yy.to_json
     #zz = JSON.parse(@article.content.body)
     #@article.content.body = yy.to_json
+
+    categories = params[:article][:category_ids].reject { |c| c.empty? }
+    @categories = Category.where(id:categories)
+    @article.categories = @categories
+
     @article.save!
 
     redirect_to blog_path
@@ -75,6 +80,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:titre,:content,:slug_authors,:slug,:lang,:title,:meta_description,:indexed,:follow,:intro,:read_time,:cover,:legend_cover,:summary)
+    params.require(:article).permit(:titre,:content,:slug_authors,:slug,:lang,:title,:meta_description,:indexed,:follow,:intro,:read_time,:cover,:legend_cover,:summary,categories:[])
   end
 end
