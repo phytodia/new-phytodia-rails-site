63b1495638f8a18092252ef3fa9f72c558b10cba
thomas hunckler <th@phytodia.com>
2025-03-06 08:18:31 +0100

    change

---
+++ app/controllers/faqs_controller.rb
@@ -0,0 +1,23 @@
+class FaqsController < ApplicationController
+  def index
+    @faqs = Faq.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
