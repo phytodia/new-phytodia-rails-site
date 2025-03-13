bb15713cc001d7eb528dbf90085fb85132fa178b
thomas hunckler <th@phytodia.com>
2025-03-13 14:29:23 +0100

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,14 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    password: Rails.application.credentials.dig(:sendgrid,:api_key), # This is the secret sendgrid API key which was issued during API key creation
+    domain:'phytodia-8e139429eeac.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 end
